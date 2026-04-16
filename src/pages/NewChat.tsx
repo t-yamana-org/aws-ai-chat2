bdd83952734619685dafd8eed8835cd0770e5eba
t-yamana-mba <gannbarimonogatari@gmail.com>
2026-04-17 03:25:45 +0900

    fix: NewChat page

--- src/pages/NewChat.tsx
+++ src/pages/NewChat.tsx
@@ -1,7 +1,23 @@
 export default function NewChat() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">AIチャットアプリ</h1>
+      <div>
+        <h1 className="text-center text-3xl font-bold">test-user さん</h1>
+        <form>
+          <div className="flex flex-col justify-between gap-4 rounded-2xl border border-gray-200 p-4">
+            <textarea
+              name="message"
+              className="field-sizing-content max-h-80 w-full resize-none overflow-y-auto border-none wrap-break-words outline-none"
+              placeholder="質問を入力してください"
+            />
+            <div className="flex justify-end gap-2">
+              <button className="flex items-center justify-center rounded-md bg-emerald-600 p-2 text-white  hover:bg-emerald-700 disabled:opacity-50 disabled:hover:bg-emerald-600">
+                送信
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
