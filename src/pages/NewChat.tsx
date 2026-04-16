82266be4e9839314a5e111bb6404ee4050290ff6
t-yamana-mba <gannbarimonogatari@gmail.com>
2026-04-17 03:52:40 +0900

    feat: react-icons 導入

--- src/pages/NewChat.tsx
+++ src/pages/NewChat.tsx
@@ -1,4 +1,17 @@
+import { FaArrowUp } from "react-icons/fa";
+
 export default function NewChat() {
+  const models = [
+    {
+      id: "global.anthropic.claude-haiku-4-5-20251001-v1:0",
+      name: "Claude Haiku 4.5 v1",
+    },
+    {
+      id: "global.anthropic.claude-sonnet-4-5-20250929-v1:0",
+      name: "Claude Sonnet 4.5 v1",
+    },
+  ];
+
   return (
     <div>
       <div>
@@ -11,8 +24,18 @@ export default function NewChat() {
               placeholder="質問を入力してください"
             />
             <div className="flex justify-end gap-2">
+              <div className="flex items-center justify-center rounded-md text-sm">
+                <select name="model" className="w-full rounded-md p-2">
+                  {models.map((model) => (
+                    <option key={model.id} value={model.id}>
+                      {model.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
               <button className="flex items-center justify-center rounded-md bg-emerald-600 p-2 text-white  hover:bg-emerald-700 disabled:opacity-50 disabled:hover:bg-emerald-600">
                 送信
+                <FaArrowUp className="h-5 w-5" />
               </button>
             </div>
           </div>
